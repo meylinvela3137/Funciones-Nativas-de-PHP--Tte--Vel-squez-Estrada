e9a05c835990333482ca8e9f766d654642520b9d
Meylin Velasquez <meylinvela3137@gmail.com>
2026-03-23 13:29:49 -0600

    funcion tipo string qur reemplaza palabras o texto ya declarada

--- FuncionesNativas.php
+++ FuncionesNativas.php
@@ -2,7 +2,7 @@
 
 //----FUNCIONES DE TIPO STRING
 
-// strlen()
+// 1. strlen()
 //retorna la cantidad de caracteres de un string
 
 // Longitud de cadena cuantos espacios hay en la funcion $texto
@@ -10,7 +10,7 @@ $texto = "Hola mundo";
 echo strlen($texto);
 // resultado: 10
 
-//strtoupper()
+//2. strtoupper()
 //Convierte todos los caracteres de un string a mayúsculas.
 
 // Convertir a mayúsculas
@@ -18,10 +18,18 @@ $nombre = "meylin";
 echo strtoupper($nombre);
 // resultado: MEYLIN
 
-//strtolower()
+//3. strtolower()
 //Convierte todos los caracteres de un string a minúsculas.
 
 // Convertir a minúsculas
 $texto = "HOLA MUNDO";
 echo strtolower($texto);
 // resultado: hola mundo
+
+//4. str_repace()
+//reemplaza todas las palabras o lo que deseamos modificar de un texto por otro dentro de un string.
+
+// Reemplazar texto
+$frase = "Me gusta el café";
+echo str_replace("café", "té", $frase);
+// resultado: Me gusta el té
