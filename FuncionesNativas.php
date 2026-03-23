1396d02bffe7e83e2ae34691f92234418ab41b41
Meylin Velasquez <meylinvela3137@gmail.com>
2026-03-23 13:26:57 -0600

    funcion tipo string strtolower()

--- FuncionesNativas.php
+++ FuncionesNativas.php
@@ -5,15 +5,23 @@
 // strlen()
 //retorna la cantidad de caracteres de un string
 
-// Longitud de cadena
+// Longitud de cadena cuantos espacios hay en la funcion $texto
 $texto = "Hola mundo";
 echo strlen($texto);
 // resultado: 10
 
 //strtoupper()
-//Convierte todos los caracteres de un string a mayúsculas.}
+//Convierte todos los caracteres de un string a mayúsculas.
 
 // Convertir a mayúsculas
 $nombre = "meylin";
 echo strtoupper($nombre);
 // resultado: MEYLIN
+
+//strtolower()
+//Convierte todos los caracteres de un string a minúsculas.
+
+// Convertir a minúsculas
+$texto = "HOLA MUNDO";
+echo strtolower($texto);
+// resultado: hola mundo
