daac6a4b4a6072a674e8ff015d96ff608fb1d757
Meylin Velasquez <meylinvela3137@gmail.com>
2026-03-23 13:25:04 -0600

    funcion tipo string strtoupper()

--- FuncionesNativas.php
+++ FuncionesNativas.php
@@ -2,10 +2,18 @@
 
 //----FUNCIONES DE TIPO STRING
 
-// STRLEN()
+// strlen()
 //retorna la cantidad de caracteres de un string
 
 // Longitud de cadena
 $texto = "Hola mundo";
 echo strlen($texto);
 // resultado: 10
+
+//strtoupper()
+//Convierte todos los caracteres de un string a mayúsculas.}
+
+// Convertir a mayúsculas
+$nombre = "meylin";
+echo strtoupper($nombre);
+// resultado: MEYLIN
