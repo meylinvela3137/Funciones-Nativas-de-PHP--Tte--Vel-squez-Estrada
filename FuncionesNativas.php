a1843971981144689a5a519d576e9ed4785d3edc
Meylin Velasquez <meylinvela3137@gmail.com>
2026-03-23 13:23:09 -0600

    funcion tipo string strlen()

--- FuncionesNativas.php
+++ FuncionesNativas.php
@@ -1,2 +1,11 @@
 <?php
 
+//----FUNCIONES DE TIPO STRING
+
+// STRLEN()
+//retorna la cantidad de caracteres de un string
+
+// Longitud de cadena
+$texto = "Hola mundo";
+echo strlen($texto);
+// resultado: 10
